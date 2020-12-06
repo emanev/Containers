2684d6c971f01e92d0645616283addb0dc55addf
Emanuil Manev <emanuil.manev@gmail.com>
2020-12-06 18:04:59 +0200

    Changes

--- Data/Containers.Data.Models/Schedule.cs
+++ Data/Containers.Data.Models/Schedule.cs
@@ -5,18 +5,29 @@
     public partial class Schedule
     {
         public int Id { get; set; }
+
         public string Description { get; set; }
+
         public int ObjectTypeId { get; set; }
+
         public DateTime RaiseDate { get; set; }
+
         public int SrsobjectId { get; set; }
+
         public int CityId { get; set; }
+
         public int DistrictId { get; set; }
+
         public TimeSpan RaiseTimeFrom { get; set; }
+
         public TimeSpan RaiseTimeTo { get; set; }
 
         public virtual City City { get; set; }
+
         public virtual District District { get; set; }
+
         public virtual ObjectTypes ObjectType { get; set; }
+
         public virtual SrsobjectIndustrial Srsobject { get; set; }
     }
 }
